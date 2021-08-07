16c96c98fe6abd437f6c91afab622f60b33f00d1
[Martijen] <jennifer_martin@outlook.fr>
2021-08-07 16:42:34 +0200

    Initial commit

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+
+    [SerializeField] AudioClip BreakSound;
+
+    // Cached reference
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        DestroyBlock();
+    }
+
+    private void DestroyBlock()
+    {
+        AudioSource.PlayClipAtPoint(BreakSound, Camera.main.transform.position);
+        Destroy(gameObject);
+        level.BlockDestroyed();
+    }
+}
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+
+    // Parameters
+    [SerializeField] int breakableBlocks; //Serialize for debugging purposes
+
+    // Cached reference
+    SceneLoader sceneloader;
+
+    private void Start()
+    {
+        sceneloader = FindObjectOfType<SceneLoader>();
+    }
+
+    public void CountBreakableBlocks()
+    {
+        breakableBlocks++;
+    }
+
+    public void BlockDestroyed()
+    {
+        breakableBlocks--;
+        if(breakableBlocks <= 0)
+        {
+            sceneloader.LoadNextScene();
+        }
+    }
+}
